183eb695135ab1dc470360bc35107dd1753f58b3
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-04-01 19:47:00 +0300

    first commit

---
+++ functions/math.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    answer = a+b
+    return answer
+
+def subtract(e,f):
+    answer = e-f
+    return answer
+
+def divide (c,d):
+    answer = c/d
+    return answer
+
+def mutiply(g,h):
+    answer =g*h
+    return answer
+
+def remainder(b,t):
+    answer = b%t
+    return answer
